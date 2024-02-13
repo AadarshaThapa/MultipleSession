d5611dbb9007908e66380480a2700fee1f5ea5e0
Aadarsha <aadarshaandkazi@gmail.com>
2024-02-13 18:05:07 +0545

    integrated indexedDb and login authorize on match

--- app.js
+++ app.js
@@ -1,49 +1,54 @@
 var app = angular.module('myApp', ['ngRoute']);
 
-app.config(['$routeProvider', function ($routeProvider) {
+app.config(['$routeProvider', function ($routeProvider, $route) {
     $routeProvider
         .when('/login', {
             templateUrl: 'login.html'
-
         })
         .when('/index', {
             templateUrl: 'index.html'
-
         })
-
         .otherwise({
             redirectTo: '/login'
         });
 
 }]);
 
+app.controller('SessionController', ['$scope', '$location', '$route', 'SessionService', function ($scope, $route, $location, SessionService) {
 
+    var request = indexedDB.open("LoginCredentialsDB", 1);
 
+    request.onupgradeneeded = function (event) {
+        var db = event.target.result;
+        var objectStore = db.createObjectStore("loginCredentials", { keyPath: "id", autoIncrement: true });
 
-
-
-
-app.factory('SessionService', function () {
-    return {
-        startSession: function (accountId, sessionData) {
-            sessionStorage.setItem(accountId, JSON.stringify(sessionData));
-        },
-        endSession: function (accountId) {
-            sessionStorage.removeItem(accountId);
-        },
-        getSessionData: function (accountId) {
-            var sessionData = sessionStorage.getItem(accountId);
-            return sessionData ? JSON.parse(sessionData) : null;
-        }
+        objectStore.add({
+            username: "test1", password: "test1", sessionData: {
+                data: "User1 Logged"
+            }
+        });
+        objectStore.add({
+            username: "test2", password: "test2", sessionData: {
+                data: "User2 Logged"
+            }
+        });
+        objectStore.add({
+            username: "test3", password: "test3", sessionData: {
+                data: "User3 Logged"
+            }
+        });
     };
-});
 
-app.controller('SessionController', ['$scope', '$location', 'SessionService', function ($scope, $location, SessionService) {
+    $scope.isLoggedIn = function () {
+        return $scope.currentAccountId !== null;
+    };
+
     $scope.currentAccountId = null;
     $scope.activeSession = {};
     $scope.loginData = {
         "username": "",
-        "password": ""
+        "password": "",
+        "isLoggedin": "false"
     };
 
     var initialAccounts = [
@@ -60,20 +65,25 @@ app.controller('SessionController', ['$scope', '$location', 'SessionService', fu
     ];
 
     $scope.loginButton = function () {
-        console.log("Login button clicked")
+        $location.path('/index');
 
-        // $scope.startSession($scope.currentAccountId, $scope.loginData);
-        // sessionStorage.setItem(loginData, JSON.stringify($scope.loginData));
         var sessionData = {
             username: $scope.loginData.username,
-            password: $scope.loginData.password
+            password: $scope.loginData.password,
+            isLoggedin: "true",
         };
+        SessionService.startSession(accountId, sessionData)
 
-        SessionService.startSession($scope.currentAccountId, sessionData);
-
-        $location.path('/index');
-        // $window.location.href = 'index.html';
+        // SessionService.checkCredentials(sessionData).then(function (accountId) {
+        //     if (accountId) {
+        //         $scope.startSession(accountId, sessionData);
+        //     } else {
+        //         console.error("Invalid username or password!");
+        //         alert("Invalid username or password!");
+        //     }
+        // });
     };
+
 
     sessionStorage.setItem('Accounts', JSON.stringify(initialAccounts));
 
@@ -95,12 +105,66 @@ app.controller('SessionController', ['$scope', '$location', 'SessionService', fu
 
     };
 
-    // $scope.loginButton = function () {
-    //     $scope.startSession($scope.currentAccountId, {});
-    //     $location.path('/index');
-
-    // };
-
-
-
 }]);
+
+
+app.factory('SessionService', ['$q', function ($q) {
+    return {
+        startSession: function (accountId, sessionData) {
+            sessionStorage.setItem(accountId, JSON.stringify(sessionData));
+        },
+        endSession: function (accountId) {
+            sessionStorage.removeItem(accountId);
+        },
+        getSessionData: function (accountId) {
+            var sessionData = sessionStorage.getItem(accountId);
+            return sessionData ? JSON.parse(sessionData) : null;
+        },
+
+
+        // checkCredentials: function (loginData) {
+        //     var deferred = $q.defer();
+        //     var request = indexedDB.open("LoginCredentialsDB");
+
+        //     request.onsuccess = function (event) {
+        //         var db = event.target.result;
+        //         var transaction = db.transaction(["loginCredentials"], "readonly");
+        //         var objectStore = transaction.objectStore("loginCredentials");
+        //         var index = objectStore.index("username");
+
+        //         var getRequest = index.get(loginData.username);
+        //         getRequest.onsuccess = function (event) {
+        //             var credential = event.target.result;
+        //             if (credential && credential.password === loginData.password) {
+        //                 deferred.resolve(credential.id);
+        //             } else {
+        //                 deferred.resolve(null);
+        //             }
+        //         };
+
+        //         getRequest.onerror = function (event) {
+        //             console.error("Error retrieving data from indexedDB:", event.target.error);
+        //             deferred.resolve(null);
+        //         };
+
+        //         transaction.oncomplete = function (event) {
+        //             db.close();
+        //         };
+        //     };
+
+        //     request.onerror = function (event) {
+        //         console.error("Error", event.target.error);
+        //         deferred.resolve(null);
+        //     };
+
+        //     return deferred.promise;
+        // }
+    };
+}]);
+
+
+
+
+
+
+
